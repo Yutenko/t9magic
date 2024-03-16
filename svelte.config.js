4cb84b1575f1513e3eee0bf80e31092a1ccd93d1
Yutenko <meisteremka@googlemail.com>
2024-03-16 21:22:28 +0100

    .....

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
-//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 //import adapter from '@sveltejs/adapter-static';
 //import adapter from '@sveltejs/adapter-vercel';
-import adapter from '@sveltejs/adapter-node';
+//import adapter from '@sveltejs/adapter-node';
 
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
